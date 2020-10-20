5be2a656618eda04175889d2c7e4d9a0329b67c7
xjl <18253040259@163.com>
2020-10-20 13:24:36 +0800

    给Chapter结构体的字段添加了标签

--- dome01/dome01.go
+++ dome01/dome01.go
@@ -2,7 +2,7 @@ package dome01
 
 //Chapter 章节
 type Chapter struct {
-	ID          int
-	ChapterName string
-	ChapterURL  string
+	ID          int    `json:"id,omitempty"`
+	ChapterName string `json:"chapter_name,omitempty"`
+	ChapterURL  string `json:"chapter_url,omitempty"`
 }
